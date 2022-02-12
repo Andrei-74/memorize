3e147629888ab1d27f1f43e67de7ca7a7e0786b2
Andrei <registr740@yandex.ru>
2022-02-12 15:50:22 +0500

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Андрей Костицин on 12.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
